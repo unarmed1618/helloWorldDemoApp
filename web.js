79728a37df9fdc74068d5765705187e09d61c40d
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2015-05-08 02:42:56 -0400

    Create, Read

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ var app = express();
 /*This can be used to prevent users from getting to 
  * things they aren't supposed to. 
 */
-var pages = ["register","verify","admin"];
+var pages = ["register","confirm","admin"];
 console.log("Finished initialization");
 function renderJadeFile(template, options) {
     var fn = jade.compile(template, options);
@@ -21,6 +21,7 @@ var dbstr = "mongodb://helloworld:1insecuretest@ds061188.mongolab.com:61188/hero
 app.set('db-uri',dbstr);
 app.set('views', __dirname + '/pages');
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
 
 models.defineModels(mongoose, function() {
@@ -29,23 +30,33 @@ models.defineModels(mongoose, function() {
 });
 
 app.get('/app/:dest',function(req,res){
-if(pages.indexOf(req.params.dest) != -1)
-    res.render(req.params.dest + '.jade');
-else
-    res.redirect('/404.html')
+    if(pages.indexOf(req.params.dest) != -1)
+	if(req.params.dest == 'admin')
+	    User.find({}, function(err,users){
+		if(users) res.render('admin.jade',{'users':users});
+		else      res.render('admin.jade');
+	    });
+    else
+	res.render(req.params.dest + '.jade');
+    else
+	res.redirect('/404.html')
 });
 app.post('/register',function(req,res){
-var newUser = new User(req.body.user);
-//Do some validations
-newUser.save();
-req.set({'validations' : 'someValidations'});
-res.redirect('/app/verify');
+var rp = req.body;
+console.log(rp);
+console.log(rp.user);
+    var newUser = new User(req.body.user);
+    //Do some validations
+    newUser.save();
+    //req.set({'validations' : 'someValidations'});
+    res.redirect('/app/confirm');
 });
 app.get('/', function(req,res){
-res.redirect('/app/register');
+    res.redirect('/app/register');
 });
-//app.get('/:dest')
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
