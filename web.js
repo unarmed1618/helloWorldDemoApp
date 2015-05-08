93e9bfc3969d19d6bc2b95b742abd0e85d83ffce
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2015-05-08 00:54:33 -0400

    disabled mongo in order to fix things, got the scaffolding and theming done. Now to add content.

--- web.js
+++ web.js
@@ -11,24 +11,25 @@ var app = express();
 //This can be used to prevent users from getting to 
 // things they aren't supposed to.
 var pages = ["register","verify","admin"];
+console.log("Finished initialization");
 function renderJadeFile(template, options) {
     var fn = jade.compile(template, options);
     return fn(options.locals);
 }
-var dbstr = "mongodb://heroku_app36645575:1insecuretest@ds061188.mongolab.com:61188/heroku_app36645575"
+var dbstr = "mongodb://helloworld:1insecuretest@ds061188.mongolab.com:61188/heroku_app36645575"
 
 app.set('views', __dirname + '/pages');
 app.use(bodyparser.json());
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/static'));
+/*
 models.defineModels(mongoose, function() {
     app.User = User = mongoose.model('User');
     db = mongoose.connect(app.set('db-uri'));
 });
-
+*/
 app.get('/app/:dest',function(req,res){
-if(pages.contains(dest))
-    res.render('shared/theme',{'page' : dest});
+if(pages.indexOf(req.params.dest) != -1)
+    res.render(req.params.dest + '.jade');
 else
     res.redirect('/404.html')
 });
@@ -36,7 +37,11 @@ app.post('/register',function(req,res){
 var newUser = new User(req.body.user);
 //Do some validations
 newUser.save();
-res.redirect('/app/verify',{validations : 'someValidations'});
+req.set({'validations' : 'someValidations'});
+res.redirect('/app/verify');
+});
+app.get('/', function(req,res){
+res.redirect('/app/register');
 });
 //app.get('/:dest')
 var port = process.env.PORT || 5000;
